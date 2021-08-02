a7b0bcf231357d0cad08739a8cc27bfafcaeec9c
viktoriiadron <88160860+viktoriiadron@users.noreply.github.com>
2021-08-02 23:50:56 +0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -4,9 +4,8 @@ const ingredients = {
   "smoothie": ["banana", "orange", "sugar"],
   "matcha frappe": ["matcha", "milk", "ice"]
 };
-
 let ul = document.querySelector('#menu');
-
+  
 ul.addEventListener('click', function menu(e) {
   let li = e.target;
   let key = e.target.innerHTML;
